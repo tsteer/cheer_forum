193e42e3a35122bc13472d640436fce8e7d325f7
Tasmin Steer <tasminsteer@hotmail.com>
2017-08-31 15:53:30 +0100

    Tweak forum post table

--- db/migrate/20170831131815_create_forum_posts.rb
+++ db/migrate/20170831131815_create_forum_posts.rb
@@ -1,11 +1,10 @@
 class CreateForumPosts < ActiveRecord::Migration[5.1]
   def change
     create_table :forum_posts do |t|
-      t.string :message
-      t.integer :forum_thread_id
+      t.string :message, null: false
+      t.references :forum_thread, foreign_key: true
 
       t.timestamps
     end
-    add_foreign_key :forum_posts, :forum_threads
   end
 end
