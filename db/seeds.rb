2043b57dbd71963d0f26337ea1a817315c8f65c1
Tasmin Steer <tasminsteer@hotmail.com>
2017-09-18 11:23:56 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user_1 = User.create(username: 'Testusername', email: 'test@test.com', date_of_birth: '1990-08-01')
+forum_category_1 = ForumCategory.create(title: 'Test category title')
+ForumThread.create(title: 'Forum thread title', forum_category: forum_category_1, user: user_1)
