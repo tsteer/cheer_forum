ee25ff42ead6f14fa14ab5831f40b7eb593d5ffd
Tasmin Steer <tasminsteer@hotmail.com>
2017-09-18 11:25:00 +0100

    Add tests for creating new post

---
+++ spec/features/forum_posts/create_forum_post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'creating a forum post' do
+  let(:user_1) { User.create(username: 'Testusername', email: 'test@test.com', date_of_birth: '1990-08-01') }
+  let(:forum_category_1) { ForumCategory.create(title: 'Test category title') }
+  let(:forum_thread_1) { ForumThread.create(title: 'Forum thread title', forum_category: forum_category_1, user: user_1) }
+
+  context 'with valid details' do
+    it 'creates the forum post' do
+      visit forum_thread_path(forum_thread_1)
+
+      fill_in 'Message', with: 'Test post message'
+      click_on 'Post'
+
+      expect(page).to have_content 'Message posted'
+    end
+  end
+end
