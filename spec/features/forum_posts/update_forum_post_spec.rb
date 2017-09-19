ccef2ae5d0db0d89ca59894aecad9b4de6a213c3
Tasmin Steer <tasminsteer@hotmail.com>
2017-09-19 10:16:47 +0100

    Update post view

--- spec/features/forum_posts/update_forum_post_spec.rb
+++ spec/features/forum_posts/update_forum_post_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.feature 'updating a thread' do
-  context 'with no thread' do
+RSpec.feature 'updating a post' do
+  context 'with no post' do
     it 'returns an error' do
-      visit edit_forum_thread_path(id: 89303)
+      visit edit_forum_post_path(id: 2332)
 
       expect(page).to have_content 'not found'
     end
@@ -13,12 +13,17 @@ RSpec.feature 'updating a thread' do
     let(:user_1) { User.create(username: 'Testusername', email: 'test@test.com', date_of_birth: '1990-08-01') }
     let(:forum_category_1) { ForumCategory.create(title: 'Test category title') }
     let(:forum_thread_1) { ForumThread.create(title: 'Forum thread title', forum_category: forum_category_1, user: user_1) }
+    let(:forum_post_1) { ForumPost.create(message: 'Forum post message 1', forum_thread: forum_thread_1, user: user_1) }
 
+    before do
+      forum_post_1
+    end
+    
     it 'returns an error' do
-      visit edit_forum_thread_path(forum_thread_1)
+      visit edit_forum_post_path(forum_post_1)
 
-      fill_in 'Title', with: ''
-      click_on 'Update thread'
+      fill_in 'Message', with: ''
+      click_on 'Update post'
 
       expect(page).to have_content 'Invalid details'
     end
@@ -28,14 +33,19 @@ RSpec.feature 'updating a thread' do
     let(:user_1) { User.create(username: 'Testusername', email: 'test@test.com', date_of_birth: '1990-08-01') }
     let(:forum_category_1) { ForumCategory.create(title: 'Test category title') }
     let(:forum_thread_1) { ForumThread.create(title: 'Forum thread title', forum_category: forum_category_1, user: user_1) }
+    let(:forum_post_1) { ForumPost.create(message: 'Forum post message 1', forum_thread: forum_thread_1, user: user_1) }
 
-    it 'updates the thread name' do
-      visit edit_forum_thread_path(forum_thread_1)
+    before do
+      forum_post_1
+    end
+    
+    it 'updates the post' do
+      visit edit_forum_post_path(forum_post_1)
 
-      fill_in 'Title', with: 'New forum thread title'
-      click_on 'Update thread'
+      fill_in 'Message', with: 'New forum post message'
+      click_on 'Update post'
 
-      expect(page).to have_content 'Thread updated'
+      expect(page).to have_content 'Post updated'
     end
   end
 end
