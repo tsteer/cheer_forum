2ea31ef19c169090ca9eafa65cb7536af98cfdd4
Tasmin Steer <tasminsteer@hotmail.com>
2017-09-26 23:07:39 +0100

    Add category index layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,24 @@ ForumThread.create(
   forum_category: forum_category_1,
   user: user_1
 )
+
+Article.create(
+  title: 'Title 1',
+  subheading: 'This is a subheading 1',
+  author: 'Article author 1',
+  article_text: 'Article text sdkjfhkdh kajdsflj a.kjdfksj'
+)
+
+Article.create(
+  title: 'Title 2',
+  subheading: 'This is a subheading 2',
+  author: 'Article author 2',
+  article_text: 'Article text sdkjfhkdh kajdsflj a.kjdfksj'
+)
+
+Article.create(
+  title: 'Title 3',
+  subheading: 'This is a subheading 3',
+  author: 'Article author 3',
+  article_text: 'Article text sdkjfhkdh kajdsflj a.kjdfksj'
+)
