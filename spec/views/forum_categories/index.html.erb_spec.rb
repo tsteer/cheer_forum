7ed8efe21dc9b442255739ae47191a7585d80a0a
Tasmin Steer <tasminsteer@hotmail.com>
2017-09-27 14:24:50 +0100

    Move lets

--- spec/views/forum_categories/index.html.erb_spec.rb
+++ spec/views/forum_categories/index.html.erb_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe 'forum_categories/index' do
   let(:user_1) { User.create(username: 'Testusername', email: 'test@test.com', date_of_birth: '1990-08-01', password: 'password', password_confirmation: 'password') }
   let(:forum_category_1) { ForumCategory.create(title: 'Category name 1') }
   let(:forum_category_2) { ForumCategory.create(title: 'Category name 2') }
-  let(:forum_thread_1) { ForumThread.create(title: 'Forum thread title 1', forum_category: forum_category_1, user: user_1) }
-  let(:forum_thread_2) { ForumThread.create(title: 'Forum thread title 2', forum_category: forum_category_2, user: user_1) }
-  
+
   context 'with no categories' do
     it 'displays a message' do
       assign(:forum_categories, [])
@@ -29,6 +27,9 @@ RSpec.describe 'forum_categories/index' do
   end
 
   context 'with 2 categories and 2 threads' do
+    let(:forum_thread_1) { ForumThread.create(title: 'Forum thread title 1', forum_category: forum_category_1, user: user_1) }
+    let(:forum_thread_2) { ForumThread.create(title: 'Forum thread title 2', forum_category: forum_category_2, user: user_1) }
+  
     it 'displays the categories and threads' do
       assign(:forum_categories, [forum_category_1, forum_category_2])
       assign(:forum_threads, [forum_thread_1, forum_thread_2])
