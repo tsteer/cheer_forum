56995e690722e8890ba1c847086f4c810b23425f
Tasmin Steer <tasminsteer@hotmail.com>
2017-09-28 16:15:20 +0100

    Select last 3 updated at

--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -3,4 +3,8 @@ class ForumThread < ApplicationRecord
   belongs_to :user
   has_many :forum_posts, dependent: :destroy
   validates_presence_of :title, :forum_category, :user
+
+  def self.latest
+    order(updated_at: :desc).limit(3)
+  end
 end
