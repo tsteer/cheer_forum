4d983967c7c0b46a692c749138ff7f2078c44432
Tasmin Steer <tasminsteer@hotmail.com>
2017-09-28 13:54:27 +0100

    Remove binding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
     if @user.save
       log_in(@user)
       redirect_to user_path(@user), flash: { success: 'User created' }
