6d1abfb585e65ab7c0cc0bb13b4b3b79350a9906
Tasmin Steer <tasminsteer@hotmail.com>
2017-09-29 11:34:50 +0100

    Change home article index order by

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   validates_presence_of :author, :title, :subheading, :article_text
 
   def self.latest
-    order(updated_at: :desc).limit(3)
+    order(created_at: :desc).limit(3)
   end
 end
